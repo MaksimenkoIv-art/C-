dc514c9b8435dd2dc2199c7c683be724a64bb39f
MaksimenkoIv-art <disenbeats@gmail.com>
2025-10-20 23:10:13 +1000

    Add files via upload

---
+++ kt9 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp47
+{
+    internal class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            Console.Write("До: ");
+            foreach (int num in numbers)
+                Console.Write(num + " ");
+            Console.WriteLine();
+
+            fixed (int* p = numbers)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                    *(p + i) += 10;
+            }
+
+            Console.Write("После: ");
+            foreach (int num in numbers)
+                Console.Write(num + " ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ kt9 2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp48
+{
+    internal class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            int x = 50;
+            Console.WriteLine($"Значение x до изменения: {x}");
+
+            int* p = &x;
+            int** pp = &p;
+
+            Console.WriteLine($"Адрес переменной x: 0x{(ulong)&x:X}");
+            Console.WriteLine($"Адрес указателя p: 0x{(ulong)&p:X}");
+
+            **pp = 999;
+            Console.WriteLine($"Значение x после изменения через pp: {x}");
+        }
+    }
+}
